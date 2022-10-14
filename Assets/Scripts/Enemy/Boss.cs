324f86da4e55a3a1a0834a99bfc6a101b7687cc5
WakanaIdeta <k021g1162@g.neec.ac.jp>
2022-10-14 13:11:41 +0900

    ボスに攻撃が当たるようにした

--- Assets/Scripts/Enemy/Boss.cs
+++ Assets/Scripts/Enemy/Boss.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
+    private GameManager gameManager;
+
+    public float hp;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject managerObj = GameObject.Find("GameManager");
+        gameManager = managerObj.GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(hp <= 0)
+        {
+            gameManager.isClear = true;
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Bullet")
+        {
+            hp--;
+            Destroy(collision.gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isClear;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isClear = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
