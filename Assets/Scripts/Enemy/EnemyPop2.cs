23cb5a04dade14f0a513b768beeb7dcbd28616c9
WakanaIdeta <k021g1162@g.neec.ac.jp>
2022-11-08 14:08:01 +0900

    タイトルとステージセレクトシーン追加

---
+++ Assets/Scripts/Enemy/EnemyPop2.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class EnemyPop2 : MonoBehaviour
+{
+    private TextAsset csvFile; // CSVファイル
+    private List<string[]> csvDatas = new List<string[]>(); // CSVの中身を入れるリスト;
+
+    private StringReader reader;
+
+    private String line;
+
+    private int listCount;
+
+    private Vector3 pos;
+
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        csvFile = Resources.Load("enemypop2") as TextAsset; // Resouces下のCSV読み込み
+        reader = new StringReader(csvFile.text);
+
+        StartCoroutine(EnemyPos());
+    }
+
+    private IEnumerator EnemyPos()
+    {
+        while (reader.Peek() != -1)
+        {
+            line = reader.ReadLine(); // 一行ずつ読み込み
+            csvDatas.Add(line.Split(',')); // , 区切りでリストに追加
+
+            if (csvDatas[0][0] == "POP")
+            {
+                //Debug.Log(float.Parse(csvDatas[0][1]));
+                pos = new Vector3(float.Parse(csvDatas[0][1]), float.Parse(csvDatas[0][2]), 0);
+
+                Instantiate(enemy, pos, Quaternion.identity);
+            }
+
+            if (csvDatas[0][0] == "WAIT")
+            {
+                // 待つ(秒)
+                yield return new WaitForSeconds(float.Parse(csvDatas[0][1]));
+            }
+
+            if (csvDatas[0][0] == "f")
+            {
+                //待つ(フレーム)
+                for (var i = 0; i < float.Parse(csvDatas[0][1]); i++)
+                {
+                    yield return null;
+                }
+            }
+
+            csvDatas.Clear();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Manager/StageSelectManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StageSelectManager : MonoBehaviour
+{
+    public string stage1;
+    public string stage2;
+    public string stage3;
+
+    public int stage;
+
+    public Text stageText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stage = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if(stage == 3)
+            {
+                stage = 1;
+            }
+            else
+            {
+                stage++;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (stage == 1)
+            {
+                stage = 3;
+            }
+            else
+            {
+                stage--;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(stage == 1)
+            {
+                ChangeScene(stage1);
+            }
+            else if (stage == 2)
+            {
+                ChangeScene(stage2);
+            }
+            else if (stage == 3)
+            {
+                ChangeScene(stage3);
+            }
+        }
+
+        stageText.text = "nextstage " + stage;
+    }
+
+    public void ChangeScene(string nextScene)
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
